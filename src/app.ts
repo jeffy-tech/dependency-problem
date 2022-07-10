df34ff71c841e5f01455360a9ce4723b3b45f637
Jeff Ancel <jancel@gmail.com>
2022-07-10 10:29:05 -0400

    Install swagger, fix deprecations in ts-jest

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,18 @@
-import { getDependency } from './dependency/dependencyService';
-import express from 'express';
+import express, { Response, Request }  from 'express';
 import bodyParser from "body-parser";
 import { RegisterRoutes } from "./tsoa-generated/routes";
+import swaggerUi from "swagger-ui-express";
+
 
 export const app = express();
 
 export const createApp = () => {
+  app.use("/docs", swaggerUi.serve, async (_req: Request, res: Response) => {
+    return res.send(
+      swaggerUi.generateHTML(await import("./tsoa-generated/swagger.json"))
+    );
+  });
+
   app.use(
     bodyParser.urlencoded({
       extended: true,
